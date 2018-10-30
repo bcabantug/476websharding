82d9c54b902d6cf52db5acfb8f2f6a8f4838e3fa
cameronmcphie <quiksilver980@gmail.com>
2018-10-29 21:03:54 -0700

    check python version for init_db

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ import json
 from datetime import datetime
 from time import gmtime, strftime, time
 import click
+import sys
 
 import uuid #will be used for generating GUIDs
 
@@ -463,7 +464,8 @@ def init_db():
                 data_insert = (uuid.uuid4(), 1, 2,  'Tue, 02 Sep 2018 15:42:28 GMT', 'Post Test - Author=1 Thread=1')
                 #test data check
                 print ('Input_data: ', data_insert)
-                conn.text_factory = lambda x: unicode(x, 'utf-8', 'ignore')
+                if sys.version_info[0] < 3:
+                    conn.text_factory = lambda x: unicode(x, 'utf-8', 'ignore')
                 c.execute('INSERT INTO Posts VALUES(?, ?, ?, ?, ?)', data_insert)
                 conn.commit()
                 c.execute('SELECT * from Posts;')
